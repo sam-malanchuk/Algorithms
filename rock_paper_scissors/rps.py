6cae292501b9141331fd0d6c8b0ab6fe870dd7ef
unknown <malanchuksam@gmail.com>
2020-03-03 21:37:07 -0500

    have a working solution for rps.py but it cannot accept n, works for fixed n=3 for now

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,14 +1,24 @@
-#!/usr/bin/python
+# !/usr/bin/python
 
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  rps = ['rock', 'paper', 'scissors']
+  sets = []
+  for rock in range(n):
+    for paper in range(n):
+      for scissors in range(n):
+        sets.append([rps[rock], rps[paper], rps[scissors]])
+  return sets
 
+print(rock_paper_scissors(3))
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_plays = int(sys.argv[1])
-    print(rock_paper_scissors(num_plays))
-  else:
-    print('Usage: rps.py [num_plays]')
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_plays = int(sys.argv[1])
+#     print(rock_paper_scissors(num_plays))
+#   else:
+#     print('Usage: rps.py [num_plays]')
+
+# Understand
+# 
